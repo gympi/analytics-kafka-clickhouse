c90566d25730f43b972088e604ced188bd70def9
gympi <p.arhipov@caredstar.ru>
2019-03-04 19:45:04 +0300

    Table sorted

--- app/src/http_server/handlers/dashboard_handler.py
+++ app/src/http_server/handlers/dashboard_handler.py
@@ -9,8 +9,12 @@ class DashboardHandler(Base):
     _template_file = 'dashboard.html'
 
     _default_params = {
+        'draw': 1,
         'page': 1,
-        'limit': 10
+        'limit': 10,
+        'start': 10,
+        'length': 10,
+        'order': None
     }
 
     def get(self):
@@ -27,13 +31,35 @@ class DashboardHandler(Base):
             except Exception:
                 params[k] = self.get_argument(k, v)
 
-        offset = params['limit'] * (params['page'] - 1)
+        params['page'] = params['draw']
+        params['limit'] = params['length']
+
+        offset = params['start']
+        oder_col = self.get_argument('order[0][column]', None)
+        oder_type = self.get_argument('order[0][dir]', None)
+
         query = session().query(VisitStat)
 
-        self.on_write_page(template=self._template_file,
-                           params={'title': 'Analytics Dashboard',
-                                   'cols': VisitStat.__table__.columns.keys(),
-                                   'items': query.order_by(VisitStat.timestamp.desc()).limit(params['limit']).offset(offset).all(),
-                                   'count': int(query.count()),
-                                   'offset': offset,
-                                   **params})
+        if oder_col:
+            if oder_type == 'desc':
+                cols = list(VisitStat.__table__.columns.keys())
+                query = query.order_by(getattr(VisitStat, cols[int(oder_col)-1]).desc())
+        else:
+            query = query.order_by(VisitStat.timestamp.desc())
+
+        if self.is_ajax_request():
+            self.on_write_json({
+                "draw": params['draw'],
+                "recordsTotal": int(query.count()),
+                "recordsFiltered": int(query.count()),
+                "data": list(list(item.to_dict().values()) for item in query.limit(params['limit']).offset(offset).all())
+            })
+        else:
+            self.on_write_page(template=self._template_file,
+                               params={'title': 'Analytics Dashboard',
+                                       'cols': VisitStat.__table__.columns.keys(),
+                                       'items': query.limit(params['limit']).offset(offset).all(),
+                                       'count': int(query.count()),
+                                       'offset': offset,
+                                       'static_url': self.static_url,
+                                       **params})
--- app/src/server.py
+++ app/src/server.py
@@ -3,6 +3,8 @@
 # chmod +x ./server.py
 
 import multiprocessing
+import os
+
 import tornado.ioloop
 import tornado.web
 import tornado.httpserver
@@ -11,8 +13,16 @@ from http_server.actions import actions
 from libs.system_environment import SystemEnvironment
 
 
+settings = {
+    "static_path": os.path.join(os.path.dirname(__file__), "./http_server/static/"),
+    "cookie_secret": "__GENERATE_RANDOM_VALUE_HERE__",
+    # "login_url": "/login",
+    "xsrf_cookies": True,
+}
+
+
 def make_app():
-    return tornado.web.Application(actions)
+    return tornado.web.Application(actions, **settings)
 
 
 if __name__ == "__main__":
