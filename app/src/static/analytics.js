a2421dcc344b6dac5bf25832ed2cd6f57a31b95d
gympi <p.arhipov@caredstar.ru>
2019-04-02 15:22:55 +0300

    Refactoring

--- app/src/static/analytics.js
+++ app/src/static/analytics.js
@@ -36,15 +36,15 @@ function buildUrl(url, parameters) {
 
 function build_params(added_params){
     return Object.assign({}, {
-            urlVisit: window.location.href,
-            timezoneClient: Intl.DateTimeFormat().resolvedOptions().timeZone,
-            sessionStartClient: sessionStartClient,
-            screenWidth: windowRect().width,
-            screenHeight: windowRect().height,
-            width: windowRect().width,
-            height: windowRect().height,
+        urlVisit: window.location.href,
+        timezoneClient: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        sessionStartClient: sessionStartClient,
+        screenWidth: windowRect().width,
+        screenHeight: windowRect().height,
+        width: windowRect().width,
+        height: windowRect().height,
 
-        }, added_params);
+    }, added_params);
 }
 
 function send_tick(parameters){
@@ -58,26 +58,26 @@ function send_tick(parameters){
         credentials: 'include',
         cache: 'no-cache',
         mode: 'cors'})
-      .then(function(response){
-          if (response.status !== 200) {
-            console.log('Looks like there was a problem. Status Code: ' +
-              response.status);
-            return;
-          }
+        .then(function(response){
+            if (response.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' +
+                    response.status);
+                return;
+            }
 
-          var contentType = response.headers.get("content-type");
+            var contentType = response.headers.get("content-type");
             if(contentType && contentType.includes("application/json")) {
-              return response.json();
+                return response.json();
             }
             throw new TypeError("Oops, we haven't got JSON!");
-      })
-      .then(function(json) {
-              sessionUidClient=json.sessionUidClient
-          }
-      )
-      .catch(function(err) {
-        console.log('Fetch Error', err);
-      });
+        })
+        .then(function(json) {
+                sessionUidClient=json.sessionUidClient
+            }
+        )
+        .catch(function(err) {
+            console.log('Fetch Error', err);
+        });
 }
 
 // Init environment
@@ -100,12 +100,12 @@ function isFloat(n){
 
 function windowRect() {
     var width = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
 
     var height = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
+        || document.documentElement.clientHeight
+        || document.body.clientHeight;
 
     return {
         width: width,
@@ -159,24 +159,24 @@ function createAnalyticImage(){
 
 // Load Pge
 function afterLoad() {
-  console.log('Send tick');
-  send_tick(build_params({state: 'open'}));
+    console.log('Send tick');
+    send_tick(build_params({state: 'open'}));
 }
 
 el.onload = el.onerror = function() {
-  if (!this.executed) { // executed only once
-    this.executed = true;
-    afterLoad();
-  }
+    if (!this.executed) { // executed only once
+        this.executed = true;
+        afterLoad();
+    }
 };
 
 el.onreadystatechange = function() {
-  var self = this;
-  if (this.readyState === "complete" || this.readyState === "loaded") {
-    setTimeout(function() {
-      self.onload()
-    }, 0); // Save "this" for onload
-  }
+    var self = this;
+    if (this.readyState === "complete" || this.readyState === "loaded") {
+        setTimeout(function() {
+            self.onload()
+        }, 0); // Save "this" for onload
+    }
 };
 
 // Close page
@@ -218,10 +218,10 @@ AnalyticCloudFun.Environments = (function(){
     var urlParam = function (name){
         var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
         if (results==null){
-           return null;
+            return null;
         }
         else{
-           return decodeURI(results[1]) || 0;
+            return decodeURI(results[1]) || 0;
         }
     };
 
@@ -233,31 +233,17 @@ AnalyticCloudFun.Environments = (function(){
 })();
 
 function createElementFromHTML(htmlString) {
-  var div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
 
-  // Change this to div.childNodes to support multiple top-level nodes
-  return div.firstChild;
-}
-
-function mark(width, height, top, left, name_element) {
-    var element = createElementFromHTML('<div class="js-analytics-mark" style="position: absolute; left: ' + left.toString() + 'px; right: 0px; top: ' + top.toString() + 'px; height: ' + height.toString() + 'px; width: ' + width.toString() + 'px; z-index: 9999;opacity: 0.5;background: rgb(244, 78, 78); pointer-events: none;"><span style="position: absolute; top: -28px; left: 16px; font-family: Arial; font-size: 15px; color: white; background: rgb(244, 78, 78); line-height: 32px; padding: 0px 16px; display: inline-block; border-radius: 3px;">Block selection '+ name_element.toString() + '</span></div>');
-    document.getElementsByTagName("body")[0].appendChild(element);
-    return element;
-}
-
-function markView(top) {
-    rect = windowRect();
-    var width = rect.width / 100 * 95;
-    var element = createElementFromHTML('<div class="js-analytics-mark" id="markView" style="position: absolute; margin-left: auto; margin-right: auto; top: ' + top.toString() + 'px; height: 3px; width: ' + width.toString() + 'px; z-index: 9999;opacity: 0.5; background: rgb(135,206,250); pointer-events: none;"><div style="position: absolute; width: 3px; height: 23px; top: -10px; background: rgb(135,206,250); pointer-events: none;"></div><div style="position: absolute; width: 3px; height: 23px; top: -10px; left: ' + width.toString() + 'px; background: rgb(135,206,250); pointer-events: none;"></div><span style="position: absolute; top: -28px; left: 16px; font-family: Arial; font-size: 15px; color: white; background: rgb(135,206,250); line-height: 32px; padding: 0px 16px; display: inline-block; border-radius: 3px;">View mark</span></div>');
-    document.getElementsByTagName("body")[0].appendChild(element);
-    return element;
+    // Change this to div.childNodes to support multiple top-level nodes
+    return div.firstChild;
 }
 
 AnalyticCloudFun.Develop = (function () {
+    var _initDevelopEnveroment = function(){
 
-    var _initDevelopEnveroment = function(flag){
-        console.log(AnalyticCloudFun.Environments.initHashParams()['dev']);
+        //Show observed blocks
         observedElements.forEach(function(observedElement) {
             if (Number.parseInt(AnalyticCloudFun.Environments.initHashParams()['dev']) > 0)
                 observedElement.showMarkBlock();
@@ -265,14 +251,14 @@ AnalyticCloudFun.Develop = (function () {
                 observedElement.hideMarkBlock();
         });
 
-        markView();
-
-        window.addEventListener("scroll", function(event) {
-            var _viewPort = viewPort();
-            document.getElementById('markView').style.top = _viewPort.scrollTop + windowRect().height / 100 * 70  + 'px';
+        //Show mark blocks
+        observerElements.forEach(function (observerElement) {
+            if (Number.parseInt(AnalyticCloudFun.Environments.initHashParams()['dev']) > 0)
+                observerElement.showMarkBlock();
+            else
+                observerElement.hideMarkBlock();
         });
     };
-
 
     window.addEventListener("hashchange", _initDevelopEnveroment);
 })();
@@ -284,94 +270,122 @@ var observedElements = [];
 
 class ObservedElement {
 
-  constructor(element, selector) {
-    this.element = element;
-    this.selector = selector;
-    this.isView = false;
-    this.rect = element.getBoundingClientRect();
-    this._on = false;
-    this.mark = undefined;
-
-    this.element.addEventListener("eventIsViewObservedElement", function(event) {
-        console.log(event)
-    }, false);
-  }
-
-  setIsView(){
-      this.isView = true;
-
-      if (this.mark !== undefined)
-        this.mark.style.background = 'rgb(135,206,250)';
-      console.log(this.element);
-      console.log(this.isView);
-      this.element.dispatchEvent(new Event("eventIsViewObservedElement"))
-  }
-
-  showMarkBlock(){
-      if (!this._on){
-        this._on = true;
-        this.mark = mark(this.rect.width, this.rect.height, this.rect.top, this.rect.left, this.selector);
-      }
-  }
-
-  hideMarkBlock(){
-      if (this._on){
+    constructor(element, selector) {
+        this.element = element;
+        this.selector = selector;
+        this.isView = false;
+        this.rect = element.getBoundingClientRect();
         this._on = false;
-        this.mark.parentElement.removeChild(this.mark);
-      }
-  }
+        this.mark = undefined;
+
+        this.element.addEventListener("eventIsViewObservedElement", function(event) {
+            console.log(event)
+        }, false);
+    }
+
+    setIsView(){
+        this.isView = true;
+
+        if (this.mark !== undefined)
+            this.mark.style.background = 'rgb(135,206,250)';
+        console.log(this.element);
+        console.log(this.isView);
+        this.element.dispatchEvent(new Event("eventIsViewObservedElement"))
+    }
+
+    showMarkBlock(){
+        if (!this._on){
+            this._on = true;
+            this.mark = this._mark(this.rect.width, this.rect.height, this.rect.top, this.rect.left, this.selector);
+        }
+    }
+
+    hideMarkBlock(){
+        if (this._on){
+            this._on = false;
+            this.mark.parentElement.removeChild(this.mark);
+        }
+    }
+
+    _mark(width, height, top, left) {
+        var element = createElementFromHTML(
+            '<div class="js-analytics-mark" style="position: absolute; left: ' + left.toString() + 'px; right: 0px; top: ' + top.toString() + 'px; height: ' + height.toString() + 'px; width: ' + width.toString() + 'px; z-index: 9999;opacity: 0.5;background: rgb(244, 78, 78); pointer-events: none;"><span style="position: absolute; top: -28px; left: 16px; font-family: Arial; font-size: 15px; color: white; background: rgb(244, 78, 78); line-height: 32px; padding: 0px 16px; display: inline-block; border-radius: 3px;">Block selection '+ this.selector.toString() + '</span></div>'
+        );
+        document.getElementsByTagName("body")[0].appendChild(element);
+        return element;
+    }
 }
 
 var observerElements = [];
 
 class ObserverElement {
-  constructor() {
-    this.buildPosition();
+    constructor() {
+        this.buildPositionMark();
 
-    this._on = false;
-    this.mark = undefined;
-
-    document.addEventListener("scrol", this.buildPosition, false);
-  }
-
-  buildPosition(){
-    var rect = windowRect();
-    this.top = rect.height / 100 * 70;
-  }
-
-  showMarkBlock(){
-      if (!this._on){
-        this._on = true;
-        this.mark = markView(this.top);
-        window.addEventListener("scrol", function (event) {
-            this.mark.style.top = this.top + 'px';
-        }, false);
-      }
-  }
-
-  hideMarkBlock(){
-      if (this._on){
         this._on = false;
-        this.mark.parentElement.removeChild(this.mark);
-      }
-  }
+        this.mark = undefined;
+
+        var a = this;
+
+        document.addEventListener("scroll", function (){a.buildPositionMark()}, false);
+    }
+
+    buildPositionMark(){
+        var _viewPort = viewPort();
+        var _windowRect = windowRect();
+        this.top = _viewPort.scrollTop + _windowRect.height / 100 * 70;
+    }
+
+    buildScrolPositionMark(){
+        document.getElementById('markView').style.top = this.top + 'px';
+    }
+
+    showMarkBlock(){
+        if (!this._on){
+            this._on = true;
+            this.mark = this._mark(this.top);
+            var a = this;
+            window.addEventListener("scroll", function (){a.buildScrolPositionMark()}, false);
+        }
+    }
+
+    hideMarkBlock(){
+        if (this._on){
+            this._on = false;
+            this.mark.parentElement.removeChild(this.mark);
+        }
+    }
+
+    _mark(top) {
+        var rect = windowRect();
+        var width = rect.width / 100 * 95;
+        var element = createElementFromHTML(
+            '<div class="js-analytics-mark" id="markView" style="position: absolute; margin-left: auto; margin-right: auto; top: ' + top.toString() + 'px; height: 3px; width: ' + width.toString() + 'px; z-index: 9999;opacity: 0.5; background: rgb(135,206,250); pointer-events: none;"><div style="position: absolute; width: 3px; height: 23px; top: -10px; background: rgb(135,206,250); pointer-events: none;"></div><div style="position: absolute; width: 3px; height: 23px; top: -10px; left: ' + width.toString() + 'px; background: rgb(135,206,250); pointer-events: none;"></div><span style="position: absolute; top: -28px; left: 16px; font-family: Arial; font-size: 15px; color: white; background: rgb(135,206,250); line-height: 32px; padding: 0px 16px; display: inline-block; border-radius: 3px;">View mark</span></div>'
+        );
+        document.getElementsByTagName("body")[0].appendChild(element);
+        return element;
+    }
 }
 
 (function () {
-  if (!Array.prototype.forEach) {
-    Array.prototype.forEach = function forEach (callback, thisArg) {
-      if (typeof callback !== 'function') {
-        throw new TypeError(callback + ' is not a function');
-      }
-      var array = this;
-      thisArg = thisArg || this;
-      for (var i = 0, l = array.length; i !== l; ++i) {
-        callback.call(thisArg, array[i], i, array);
-      }
-    };
-  }
+    if (!Array.prototype.forEach) {
+        Array.prototype.forEach = function forEach (callback, thisArg) {
+            if (typeof callback !== 'function') {
+                throw new TypeError(callback + ' is not a function');
+            }
+            var array = this;
+            thisArg = thisArg || this;
+            for (var i = 0, l = array.length; i !== l; ++i) {
+                callback.call(thisArg, array[i], i, array);
+            }
+        };
+    }
 })();
 
+//Init marks
+observerElements.push(new ObserverElement());
+
+//Init observed elements
 selectors.forEach(function(selector) {
     var elements = document.querySelectorAll(selector);
 
@@ -380,14 +394,12 @@ selectors.forEach(function(selector) {
     });
 });
 
-observerElements.push(new ObserverElement());
 
 window.addEventListener("scroll", function(event) {
     observerElements.forEach(function (observerElement) {
         observedElements.filter(elementItem => elementItem.isView === false).forEach(function (elementItem) {
             var elementRect = elementItem.rect;
 
-            console.log(elementRect.bottom);
             //if(elementRect.bottom < viewPort.scrollBottom && elementRect.top > viewPort.scrollTop){
             if(elementRect.top < observerElement.top){
                 elementItem.setIsView();
@@ -397,6 +409,6 @@ window.addEventListener("scroll", function(event) {
 }, false);
 
 
-window.addEventListener("eventIsViewObservedElement", function(event) {
-    console.log(event)
-}, false);
+// window.addEventListener("eventIsViewObservedElement", function(event) {
+//     console.log(event)
+// }, false);
